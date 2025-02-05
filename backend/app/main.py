d8f717bb29ff62239bee3ce490595abd84706f61
rodrigoestigoni <rodrigo.estigoni@gmail.com>
2025-02-05 07:37:47 -0300

    correção backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
+import sys
+import os
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
 from app.database import engine, Base
+
 from . import routes
 
 # Cria as tabelas
