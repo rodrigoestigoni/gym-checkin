cfe08c9e997779f4567aa81d5875ff6a79cab797
rodrigoestigoni <rodrigo.estigoni@gmail.com>
2025-02-05 07:38:06 -0300

    correção backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.dirname(SCRIPT_DIR))
 
 from app.database import engine, Base
 
-from . import routes
+from app import routes
 
 # Cria as tabelas
 Base.metadata.create_all(bind=engine)
